e1acb58f65ca0efcacf8948e5a533d591c34a533
Tyler Cave <59815381+tylts@users.noreply.github.com>
2024-07-09 12:21:54 -0500

    made it more simplers

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
-(function geoFindMe() {
-  const status = document.querySelector('#status');
-  const mapLink = document.querySelector('#map-link');
-  const altitudeText = document.querySelector('#altitude-text');
-  const speedText = document.querySelector('#speed-text');
+const locText = document.querySelector('#location-text');
+const speedText = document.querySelector('#speed-text');
 
-  mapLink.href = '';
-  mapLink.textContent = '';
+const watchID = navigator.geolocation.watchPosition((position) => {
+  doSomething(
+    position.coords.latitude,
+    position.coords.longitude,
+    position.coords.speed
+  );
+});
 
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const altitude = position.coords.altitude;
-    const speed = position.coords.speed;
-    console.log(position.coords);
-    // console.log(navigator.geolocation.watchPosition());
-
-    status.textContent = '';
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °, Altitude: ${altitude}, Speed: ${speed}`;
-    altitudeText.textContent = `Altitude: ${altitude}`;
-    speedText.textContent = `Speed: ${speed}`;
-  }
-
-  function error() {
-    status.textContent = 'well shit';
-  }
-
-  if (!navigator.geolocation) {
-    status.textContent = 'not support';
-  } else {
-    status.textContent = 'Locating...';
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-})();
-
-// const locate = geoFindMe();
-
-// document.querySelector('#find-me').addEventListener('click', geoFindMe);
+function doSomething(lat, long, speed) {
+  locText.innerText = `Latitude: ${lat}, Longitude: ${long}`;
+  speedText.innerText = `Speed: ${speed}`;
+}
