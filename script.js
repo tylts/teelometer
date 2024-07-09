4ad2cd513a9703203d6426b98522c2bbb80404f2
Tyler Cave <59815381+tylts@users.noreply.github.com>
2024-07-09 12:47:46 -0500

    added success and error callbacks, options

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const locText = document.querySelector('#location-text');
 const speedText = document.querySelector('#speed-text');
+const uhOhText = document.querySelector('#error');
 
-const watchID = navigator.geolocation.watchPosition((position) => {
-  doSomething(
-    position.coords.latitude,
-    position.coords.longitude,
-    position.coords.speed
-  );
-});
+function success(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let speed = position.coords.speed;
 
-function doSomething(lat, long, speed) {
   locText.innerText = `Latitude: ${lat}, Longitude: ${long}`;
   speedText.innerText = `Speed: ${speed}`;
 }
+
+function error(err) {
+  uhOhText.innerText = 'UH OH';
+  console.log(err.message);
+}
+
+options = {
+  enableHighAccuracy: true,
+  timeout: 100,
+};
+
+let watchID = navigator.geolocation.watchPosition(success, error, options);
